30fe2bc138425afb7007260b68a5bf4d00c96450
biogis <biogis@users.noreply.github.com>
2018-06-22 14:06:53 +0200

    Update Bat_Finder.r

--- Bat_Finder.r
+++ Bat_Finder.r
@@ -15,6 +15,21 @@
 ###########################################################
 ###########################################################
 
+# Launch via:
+
+# #https://tonybreyal.wordpress.com/2011/11/24/source_https-sourcing-an-r-script-from-github/
+# source_https <- function(url, ...) {
+#   # load package
+#   require(RCurl)
+#   
+#   # parse and evaluate each .R script
+#   sapply(c(url, ...), function(u) {
+#     eval(parse(text = getURL(u, followlocation = TRUE, cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))), envir = .GlobalEnv)
+#   })
+# }
+# source_https("https://raw.githubusercontent.com/biogis/r/master/Bat_Finder.r")
+
+
 
 start <- Sys.time();start
 
