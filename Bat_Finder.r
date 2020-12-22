62d89b6af00d31ec47a53d7f68a97a149c54755b
eRey <biogis@users.noreply.github.com>
2020-12-22 11:10:13 +0100

    Update Bat_Finder.r

--- Bat_Finder.r
+++ Bat_Finder.r
@@ -2,9 +2,9 @@
 #########################################################
 # © eRey.ch | bioGIS; erey@biogis.ch
 # created on 2018.06.18
-# modified on 2018.06.21
-#source('./Bat_Filter_III.r')
-# https://github.com/biogis/r/blob/master/Bat_Filter_III.r
+# modified on 2020.12.22
+#source('./Bat_Filter_VI.r')
+# https://github.com/biogis/r/blob/master/Bat_Finder.r
 
 # Set your working directory
 
@@ -78,6 +78,7 @@ spectral.colors <- colorRampPalette(c('red3','darkblue','gold2','limegreen','gre
 
 
 
+
 #choose working directory with all wav files
 in.dir <- choose.dir(caption = "Select input wav folder")
 
@@ -86,147 +87,309 @@ out.dir <- choose.dir(caption = "Select output wav folder")
 
 setwd(out.dir)
 
+start <- Sys.time();start
 
 fn.f <- file.path(in.dir)
-fns <- list.files(fn.f,pattern='.WAV$',all.files=T,full.names=T,recursive=T,include.dirs=T) # store all wave files in the working directory
+fns <- list.files(fn.f,pattern='.wav$',all.files=T,full.names=T,recursive=T,include.dirs=T) # store all wave files in the working directory
 print(length(fns))
 
+result <- read.csv(text='fileName, freq, Date, FullName, SNR SNE, SNR RMS, RMS, Slope, R2, median')
+result.all <- read.csv(text='fileName, freq, Date, FullName, SNR SNE, SNR RMS, RMS, Slope, R2, median')
 
-
-result <- read.csv(text='fileName,freq,Date,FullName,SNR,RMS')
-result.all <- read.csv(text='fileName,freq,Date,FullName,SNR,RMS')
 tt <- c()
 
 
-a <- readWave(fns[1])
-# str(a)
-wl <- 1024
-ovlp <- 0
-wl.half <- wl/2
-fH <- a@samp.rate
-fH.half <- fH/2
-dur <- length(a@left)/fH
-rm(a)
-#f <- fns[1]
-
-
-for(f in fns){ start.1 <- Sys.time();start.1
+for(f in fns){
+  start.1 <- Sys.time();start.1
   y <- which(f==fns)
   a <- try(readWave(f),silent=T)
-  if(class(a)=='try-error' | length(a@left)>0){
-    
-    tr <- strsplit(basename(f),"_")
-    seq.f <- tail(tr[[1]],n=1)
-    x.f <- as.integer(as.hexmode(substr(basename(seq.f), 1, 8))) # get the integer version of the HEX code
-    asofdate <- as.POSIXct(x.f, origin="1970-01-01") # convert unix EPOC time to UTC time
-    x.convert <- strptime(asofdate, format="%Y-%m-%d %H:%M:%S") # paste and format date and time in a single vector
-    Date.file <- format(x.convert, "%Y%m%d%H%M");Date.file # format date and time, add to the dtf.header
-    
-    dt <- stft.ext(f,verbose=F,ovlp=ovlp,wl=wl,dB = T,mean=T)#compute fft of the wav file
-    # str(dt)
-    
-    row <- seq(0,fH.half,by=fH/wl)/1000 # prepare new rownames with frequencies
-    col <- seq(0,dur,by=wl/fH)*1000 # prepare new colnames with time
-    rownames(dt$amp) <- row[-length(row)] # rename rows with frequencies values
-    colnames(dt$amp) <- col[-length(col)] # rename columns with time values
-    
-    dt$amp[1:10,1:10] # check matrix
+  if(class(a)!='try-error'){
+    if(length(a@left)>0){
 
-    #Signal Finder, for each frequency band, substract the 10% quantile and select the values > than 10% above the mean, else set as NA
-    rms.tot.unfilt <- -(sqrt(mean(dt$amp^2,na.rm=T)));rms.tot.unfilt
-    
-    #select data for every line, aka freq bin, or freq line
-    for(i in 1:wl.half){
-      dt$amp[i,which(dt$amp[i,]<quantile(dt$amp[i,],.1,na.rm=T))] <- NA # substract 5% quantile to avoid specific freq bin noise floor
-      mea <- mean(dt$amp[i,],na.rm=T)
-      dt$amp[i,which(dt$amp[i,]<(mea-(mea/10)))] <- NA # denoise freq line
-      
-            }
+      # use only if your sequence is saved as XXXXX_YYYYmmdd_HHMMSS, else uncomment the line asofdate <- x
+      tr <- strsplit(basename(f),"_")
+      asofdate <- strptime(paste(tr[[1]][2], tr[[1]][3], sep=' '), format="%Y%m%d %H%M%S", tz='') # paste and format date and time in a single vector
+      # asofdate <- 'x'
 
-mean.mod <- rowMeans(dt$amp,na.rm=T)
+      a <- readWave(f)
+      # str(a)
+      wl <- 1024
+      ovlp <- 50
+      wl.half <- wl/2
+      fH <- a@samp.rate
+      fH.half <- fH/2
+      dur <- length(a@left)/fH
+      wn <- 'hamming'
+      zp <- 64
+
+      a.filt <- fir(a, from = 8000, to = 150000)
+      dt <- seewave::spectro(a.filt, f=fH, wn=wn, ovlp=ovlp, wl=wl, zp=zp, plot=F)#compute fft of the wav file
+      # str(dt)
+      dtf <- as.data.table(dt$amp)
 
 
+      row <- dt$freq # prepare new rownames with frequencies
+      col <- dt$time # prepare new colnames with time
+      length(row); length(col)
+      rownames(dt$amp) <- row # rename rows with frequencies values
+      colnames(dt$amp) <- col # rename columns with time values
 
-    
-    for(j in 1:dim(dt$amp)[2]){
-      dt$amp[which(dt$amp[,j]<quantile(dt$amp[,j],.5,na.rm=T)),j] <- NA
-      mea <- mean(dt$amp[,j],na.rm=T)
-      dt$amp[which(dt$amp[,j]<(mea-(mea/10))),j] <- NA
-      
-    }
+      dt$amp[1:10,1:10] # check matrix
 
-specSum <- colSums(dt$amp,na.rm=T)
-peakBin <- colMaxs(dt$amp,na.rm=T)
+      #Signal Finder, for each frequency band, substract the 50% quantile and select the values > than 10% above the mean, else set as NA
+      rms.tot.unfilt <- -(sqrt(mean(dt$amp^2,na.rm=T)));rms.tot.unfilt
 
-    mean.mod[is.na(mean.mod)] <- min(mean.mod,na.rm=T)
-    peakBin[is.infinite(peakBin)] <- NA
-    
-    dtf.freq <- data.frame('Time'=col[-length(col)],peakBin,specSum);summary(dtf.freq)
-    
-    
-    ## the 'lm'
-    tl <- lm(peakBin ~ specSum)
-    data.tl <- summary(tl);data.tl
-    slp <- format(as.numeric(coef(tl)[2]), digits = 2);slp       ## extracting slope:
-    R2 <- paste0(format(data.tl$r.squared, digits = 2), "%");R2
-    
-    rms.tot.filt <- -(sqrt(mean(dt$amp^2,na.rm=T)));rms.tot.filt
 
-    
-    pwr <- data.frame('freq'= row[-length(row)],'amp'=mean.mod) #dataframe for power spectrum
-    
-    pwr.bat <- pwr[which(pwr$freq>15 & pwr$freq<60 | # Look for any bat in freq range 10 to 60 kHz
+      system.time(rowmea  <-  rowMeans(dt$amp, na.rm=T))
+      system.time(rowmed  <-  apply(dt$amp, 1, median, na.rm = T))
+      system.time(colmea  <-  colMeans(dt$amp, na.rm=T))
+      system.time(colmed  <-  apply(dt$amp, 2, median, na.rm = T))
+
+
+      system.time(
+        #select data for every row, aka freq bin, or freq line
+        for(frq in 1:dim(dt$amp)[1]){
+          dt$amp[frq,which(dt$amp[frq,]<rowmed[frq])] <- NA # substract 5% quantile to avoid specific freq bin noise floor
+          mea <- mean(dt$amp[frq,],na.rm=T)
+          mea.10 <- mea-(mea/10)
+          dt$amp[frq,which(dt$amp[frq,] < mea.10)] <- NA # denoise freq line
+          })
+
+      mean.mod <- rowMeans(dt$amp,na.rm=T)
+
+      system.time(
+      #select data for every column, aka time bin, or time column
+      for(tm in 1:dim(dt$amp)[2]){
+        dt$amp[which(dt$amp[,tm]<colmed[tm]),tm] <- NA
+        mea <- mean(dt$amp[,tm],na.rm=T)
+        mea.10 <- mea-(mea/10)
+        dt$amp[which(dt$amp[,tm] < mea.10),tm] <- NA
+      })
+
+      specSum <- colSums(dt$amp,na.rm=T)
+      peakBin <- colMaxs(dt$amp,na.rm=T)
+
+
+      mean.mod[is.na(mean.mod)] <- min(mean.mod,na.rm=T)
+      peakBin[is.infinite(peakBin)] <- NA
+
+      dtf.freq <- data.frame('Time'=col,peakBin,specSum);summary(dtf.freq)
+
+      ## the 'lm'
+      tl <- lm(peakBin ~ specSum)
+      data.tl <- summary(tl);data.tl
+      slp <- format(as.numeric(coef(tl)[2]), digits = 2);slp       ## extracting slope:
+      R2 <- paste0(format(data.tl$r.squared, digits = 2), "%");R2
+
+      rms.tot.filt <- -(sqrt(mean(dt$amp^2,na.rm=T)));rms.tot.filt
+
+      pwr <- data.frame('freq'= row,'amp'=mean.mod) #dataframe for power spectrum
+
+      pwr.bat <- pwr[which(pwr$freq>10 & pwr$freq<60 | # Look for any bat in freq range 10 to 60 kHz
                            pwr$freq>75 & pwr$freq<85 | # Look for rFe in freq range 75 to 85 kHz
                            pwr$freq>100 & pwr$freq<110),] # Look fir rHi in freq range 100 to 110 kHz
-    pks.i <- find_peaks(pwr.bat$amp,15) # find index with peaks values
-    pks <- subset(pwr.bat[pks.i,])#;print(pks) # subset peaks frequencies compared to rms value
-    
-    pks
-    
-
-    
-    if(length(pks$freq)>0 & slp<0){
-      
-      nse <- median(dt$amp,na.rm=T)
-      pks$snr <- pks$amp/nse
+      pks.i <- find_peaks(pwr.bat$amp,15) # find index with peaks values
+      pks <- subset(pwr.bat[pks.i,])#;print(pks) # subset peaks frequencies compared to rms value
       pks
-      
 
-      
-      
-      #### ^.^ #### ^.^ #### ^.^ #### ^.^ ####
-      # bat
-      
-      if(any((pks$snr)>1)){
-             # & rms.tot.filt>median(dt$amp,na.rm=T)
-            
-        
-        
-        output <- paste(out.dir,basename(f),sep='\\')
-        
-        
-        file.copy(f,output)
-        
-        chiro.dtf <- data.frame('fileName' = output,
-                                'freq' = pks$freq,
-                                'Date' = asofdate,
-                                'FullName' = basename(f),
-                                'SNR' = pks$snr,
-                                'RMS' = rms.tot.filt,
-                                'median' = median(dt$amp,na.rm=T))
-        
-        result <- rbind(result, chiro.dtf)
-        
-        print(paste(y,': chiro found at',pks$freq, 'in', basename(f), sep=' '))
+      if(length(pks$freq)>0 & slp<0){
+        i <- which(dt$freq<10 |
+                     dt$freq > 60 & dt$freq <75 |
+                     dt$freq > 85 & dt$freq < 100 |
+                     dt$freq > 120) # low and high pass filter
+        pks$rms <- -(sqrt(mean(pks$amp^2,na.rm=T)));pks
+
+        nse <- median(dt$amp[i,],na.rm=T)
+        pks$snr_nse <- (pks$amp/nse)^2
+        pks[which(is.na(pks$snr_nse)),'snr_nse'] <- 9999
+        rms.tot.filt
+        pks$snr_rms <- pks$amp/rms.tot.filt
+        pks[which(is.na(pks$snr_rms)),'snr_rms'] <- 9999
+        pks
+
+
+        #### ^.^ #### ^.^ #### ^.^ #### ^.^ ####
+        # bat
+
+        if(any((pks$snr_nse) < 0.6 | (pks$snr_rms) < 0.6)){
+          output <- paste(out.dir,basename(f),sep='/')
+          # file.copy(f,output)
+          chiro.dtf <- data.frame('fileName' = output,
+                                  'freq' = pks$freq,
+                                  'Date' = asofdate,
+                                  'FullName' = basename(f),
+                                  'SNR SNE' = pks$snr_nse,
+                                  'SNR RMS' = pks$snr_rms,
+                                  'RMS' = rms.tot.filt,
+                                  'Slope' = slp,
+                                  'R2' = R2,
+                                  'median' = median(dt$amp,na.rm=T)
+                                  )
+
+          result <- rbind(result, chiro.dtf)
+          result.all <- rbind(result.all, chiro.dtf)
+
+          # print(paste(y,': chiro found at',pks$freq, 'in', basename(f), sep=' '))
+          print(paste('chiro found in', basename(f), sep=' '))
+
+
+          # # plot control graph
+          # ################################
+          #
+          # freqPeak <- c()
+          # for(j in 1:dim(dt$amp)[2]){
+          #   mxm <- max(dt$amp[,j],na.rm=T)
+          #   idx <- which(dt$amp[,j]==mxm)
+          #   fpk <- ifelse(is.infinite(mxm)==T,NA,rownames(dt$amp)[idx])
+          #   freqPeak <- c(freqPeak,as.numeric(fpk))
+          # }
+          #
+          # pathName <- file.path(getwd())
+          # pdfname <- paste(pathName,paste(paste(sub("(.+)[.][^.]+$", "\\1", basename(f)),sep='_'),'pdf',sep='.'),sep='/')
+          #
+          # pdf(pdfname,paper='a4r',width=11,height=8.5)
+          #
+          # par(mfrow=c(1,1),mar=c(6,5,4,5))
+          #
+          # lab10 <- floor(seq(0,fH.half,by=5000)/1000)
+          #
+          # pp <- ggplot(dtf.freq, aes(x = specSum, y = peakBin))+
+          #   geom_point(aes(color = freqPeak),
+          #              alpha = 0.7,
+          #              size = 1.5,
+          #              na.rm=T) +
+          #   scale_color_gradient2(name="freqPeak",
+          #                         breaks = c(0, 50, 100),
+          #                         labels = c("0 kHz", "50 kHz", "100 kHz"),
+          #                         low = "tomato",
+          #                         high = "steelblue",
+          #                         mid = "gray60",
+          #                         midpoint = 50)+
+          #   theme_bw(base_size = 10)+
+          #   theme(axis.line = element_line(colour = 'grey80'),
+          #         panel.border = element_blank(),
+          #         panel.background = element_blank(),
+          #         axis.title.x=element_text(colour='grey50'),
+          #         axis.title.y=element_text(colour='grey50')
+          #   )+
+          #   geom_smooth(mapping = aes(linetype = "r2"),method = "lm",formula = y ~ x,se = T,na.rm=T,color='tomato')+
+          #   scale_linetype(name = "",
+          #                  breaks = "r2",
+          #                  labels = list(bquote(slope==.(slp))),
+          #                  guide = guide_legend(override.aes = list(linetype = 1, size = 2, color = "red")))
+          # #
+          # print(pp)
+          #
+          # dev.off()
+          # # #
+          #
+
+          # # # # PLOT Spectrogram
+          # # # ######################
+          # # #
+          # df <- as.data.frame(raster(dt$amp), xy=TRUE)
+          # head(df)
+          # df$layer.med <- df$layer-(median(df$layer,na.rm=T))
+          # rd.quartile <- as.numeric(summary(df$layer.med)[5])
+          # df$layer.med <- df$layer.med-rd.quartile
+          # df$layer.med[df$layer.med<0] <- 0
+          # df$layer.med[is.na(df$layer.med)] <- 0
+          # head(df)
+          # # #
+          # # #
+          # brks <- seq(0, fH.half, by=2000)
+          # arg.tps <- list(at=seq(0,1,by=1/(floor(dur/0.1))), labels=seq(0,dur,by=0.1))
+          # arg.freq <- list(at=seq(-1,0,by=1/(length(lab10)-1)), labels=lab10, minor=seq(-1,0,by=1/(2*(length(lab10)-1))))
+          # # #
+          # # #
+          # sp <- ggplot(df) +
+          #   geom_raster(aes(x, -y, fill=layer)) +
+          #   scale_fill_gradientn(name="[dB]",colours=jet.colors(length(brks)),na.value='white')+
+          #   theme_bw(base_size = 10)+
+          #   scale_x_continuous(breaks=arg.tps$at, labels = arg.tps$labels,sec.axis = dup_axis(name=waiver()))+
+          #   xlab('Temps [s]')+
+          #   scale_y_continuous(breaks= arg.freq$at,labels=arg.freq$labels,minor_breaks = arg.freq$minor,sec.axis = dup_axis(name=waiver())) +
+          #   ylab('FrÃÂ©quence [kHz]')+
+          #   theme(legend.position="none")+
+          #   theme(axis.text.x=element_text(colour='grey50',angle=0,hjust=1,vjust=1),
+          #         axis.title.x=element_text(colour='grey50'))+
+          #   theme(axis.text.y=element_text(colour='grey50'),
+          #         axis.title.y=element_text(colour='grey50'))+
+          #   theme(axis.line = element_line(colour = 'grey80'),
+          #         panel.grid.major = element_blank(),
+          #         panel.grid.minor = element_blank(),
+          #         panel.border = element_blank(),
+          #         axis.ticks = element_line(colour='grey80'),
+          #         panel.background = element_blank())
+          # # #
+          # # #
+          # xplot <- ggplot(df, aes(x=x, y=layer.med))+
+          #   geom_bar(stat="identity",colour='grey20')+
+          #   geom_bar(data = df,aes(y=-layer.med), stat="identity",colour='grey20')+
+          #   theme_bw(base_size = 8)+
+          #   xlab('Temps [s]')+
+          #   ylab('Ampl [dB]')+
+          #   theme(axis.line = element_line(colour = 'grey80'),
+          #         panel.grid.major = element_blank(),
+          #         panel.grid.minor = element_blank(),
+          #         panel.border = element_blank(),
+          #         panel.background = element_blank(),
+          #         axis.title.x=element_text(colour='grey50'),
+          #         axis.text.x=element_blank(),
+          #         axis.ticks.x=element_blank(),
+          #         axis.title.y=element_text(colour='grey50'),
+          #         axis.text.y=element_blank(),
+          #         axis.ticks.y=element_blank())+
+          #   ggtitle(basename(f))
+          # # #
+          # # #
+          # yplot <- ggplot(pwr, aes(x=freq, y=amp))+
+          #   geom_line()+
+          #   geom_point(data=pks,aes(x=freq,y=amp),colour='tomato')+
+          #   #  geom_text(data=pks,aes(label=freq),hjust=0.2, vjust=-0.5,colour='tomato')+
+          #   theme_bw(base_size = 10)+
+          #   xlab('FrÃÂ©quence [kHz]')+
+          #   ylab('Ampl [dB]')+
+          #   theme(axis.line = element_line(colour = 'grey80'),
+          #         panel.grid.major = element_blank(),
+          #         panel.grid.minor = element_blank(),
+          #         panel.border = element_blank(),
+          #         panel.background = element_blank(),
+          #         axis.title.x=element_text(colour='grey50'),
+          #         axis.text.x=element_blank(),
+          #         axis.ticks.x=element_blank(),
+          #         axis.title.y=element_text(colour='grey50'),
+          #         axis.text.y=element_blank(),
+          #         axis.ticks.y=element_blank())+
+          #   rotate()
+          # # #
+          # # #
+          # p <- ggarrange(xplot, pp, sp, yplot,
+          #                ncol = 2, nrow = 2,  align = "hv",
+          #                widths = c(6, 1), heights = c(1, 6),
+          #                common.legend = F)
+          # # #
+          # # #
+          # jpegName <- paste(pathName,paste(sub("(.+)[.][^.]+$", "\\1", basename(f)),'jpg',sep='.'),sep='/')
+          # jpeg(jpegName,bg='transparent',width=10500,height=3500,units='px',res=300)
+          #
+          # print(p)
+          #
+          # dev.off()
+          # # # #
         }
+      }
     }
+
+
+    print(paste(y,': seq:',Sys.time()-start.1,'; Total: ',Sys.time()-start,sep=' '))
+    tt <- c(tt,Sys.time()-start.1)
+
+    rm(output,pks,nse,pwr.bat,pwr,rms.tot.filt,r2,slp,data.tl,tl,dtf.freq,peakBin,mean.mod,
+     specSum,rms.tot.unfilt,col,row,dt,Date.file,x.convert,asofdate,x.f,seq.f,tr,y,a)
   }
-  print(paste(y,': seq:',Sys.time()-start.1,'; Total: ',Sys.time()-start,sep=' '))
-  tt <- c(tt,Sys.time()-start.1)
-  
-  rm(output,pks,nse,pwr.bat,pwr,rms.tot.filt,r2,slp,data.tl,tl,dtf.freq,peakBin,mean.mod,
-   specSum,rms.tot.unfilt,col,row,dt,Date.file,x.convert,asofdate,x.f,seq.f,tr,y,a)
 }
+
 
 
 print(' ')
